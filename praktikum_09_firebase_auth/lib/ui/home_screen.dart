4e11ce40d6e0ee8aaae400399b8596199aea05a7
Ithankkk <haikaltribugatti@gmail.com>
2024-06-12 04:19:13 +0700

    Update praktikum 13

--- praktikum_09_firebase_auth/lib/ui/home_screen.dart
+++ praktikum_09_firebase_auth/lib/ui/home_screen.dart
@@ -1,4 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart'; // Pastikan ini sudah ada
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -7,7 +7,7 @@ import 'package:praktikum_09_firebase_auth/ui/login.dart';
 final _firestore = FirebaseFirestore.instance;
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
@@ -60,6 +60,8 @@ class _HomeScreenState extends State<HomeScreen> {
             child: ListView(
               children: snapshot.data!.docs.map((DocumentSnapshot document) {
                 Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+                final titleEdc = TextEditingController(text: data['title'].toString());
+                final noteEdc = TextEditingController(text: data['note'].toString());
                 return SizedBox(
                   height: 170.0,
                   width: MediaQuery.of(context).size.width,
@@ -85,7 +87,86 @@ class _HomeScreenState extends State<HomeScreen> {
                               ),
                               GestureDetector(
                                 onTap: () {},
-                                child: const Icon(Icons.more_vert_outlined),
+                                child: PopupMenuButton<String>(
+                                  onSelected: (value) {
+                                    if (value == 'edit') {
+                                      showModalBottomSheet(
+                                        context: context,
+                                        isScrollControlled: true,
+                                        builder: (context) {
+                                          return Padding(
+                                            padding: const EdgeInsets.all(16.0),
+                                            child: Form(
+                                              key: _formKey,
+                                              child: Column(
+                                                mainAxisSize: MainAxisSize.min,
+                                                children: [
+                                                  TextFormField(
+                                                    controller: titleEdc,
+                                                  ),
+                                                  const SizedBox(height: 10.0),
+                                                  SizedBox(
+                                                    height: 300,
+                                                    child: TextFormField(
+                                                      controller: noteEdc,
+                                                      maxLines: null, // Set this
+                                                      expands: true, // and this
+                                                      keyboardType: TextInputType.multiline,
+                                                    ),
+                                                  ),
+                                                  Padding(
+                                                    padding: EdgeInsets.only(
+                                                      bottom: MediaQuery.of(context).viewInsets.bottom,
+                                                    ),
+                                                    child: SizedBox(
+                                                      width: MediaQuery.of(context).size.width,
+                                                      child: ElevatedButton(
+                                                        onPressed: () async {
+                                                          if (_formKey.currentState!.validate()) {
+                                                            try {
+                                                              await _firestore.collection('tasks').doc(document.id).update({
+                                                                'title': titleEdc.text,
+                                                                'note': noteEdc.text,
+                                                                'timestamp': FieldValue.serverTimestamp(),
+                                                              });
+                                                              ScaffoldMessenger.of(context).showSnackBar(
+                                                                const SnackBar(content: Text('Note berhasil diperbarui')),
+                                                              );
+                                                              Navigator.pop(context);
+                                                            } catch (e) {
+                                                              ScaffoldMessenger.of(context).showSnackBar(
+                                                                SnackBar(content: Text('$e')),
+                                                              );
+                                                            }
+                                                          }
+                                                        },
+                                                        child: const Text('Save'),
+                                                      ),
+                                                    ),
+                                                  ),
+                                                ],
+                                              ),
+                                            ),
+                                          );
+                                        },
+                                      );
+                                    } else if (value == 'delete') {
+                                      String documentId = document.id;
+                                      _firestore.collection('tasks').doc(documentId).delete();
+                                    }
+                                  },
+                                  itemBuilder: (BuildContext context) => [
+                                    const PopupMenuItem<String>(
+                                      value: 'edit',
+                                      child: Text('Edit'),
+                                    ),
+                                    const PopupMenuItem<String>(
+                                      value: 'delete',
+                                      child: Text('Hapus'),
+                                    ),
+                                  ],
+                                  child: Icon(Icons.more_vert_outlined),
+                                ),
                               ),
                             ],
                           ),
@@ -188,6 +269,7 @@ class _HomeScreenState extends State<HomeScreen> {
             },
           );
         },
+        child: Icon(Icons.add),
       ),
     );
   }
